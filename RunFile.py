55230c008250d07e67ab7edaceb5c8d4bdeb4eb9
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-12-18 18:05:47 -0800

    Update RunFile.py

--- RunFile.py
+++ RunFile.py
@@ -9,24 +9,23 @@ global current_time
 now = datetime.now()
 current_time = now.strftime("%H")
 print("Current Time =", str(current_time), "Hour")
-while True:
+now = datetime.now()
+current_time = now.strftime("%H")
+if 6 <= int(current_time) < 13:
+    print("Current Time =", current_time, "Hour")
+    logger.info('Trading Now...')
+    current_time = now.strftime("%H")
+    os.system('python tradeAI.py trade')
+    if current_time == 13:
+        break
+elif 13 <= int(current_time) <14 or 0<= int(current_time) < 6:
     now = datetime.now()
     current_time = now.strftime("%H")
-    if 6 <= int(current_time) < 13:
-        print("Current Time =", current_time, "Hour")
-        logger.info('Trading Now...')
-        current_time = now.strftime("%H")
-        os.system('python tradeAI.py trade')
-        if current_time == 13:
-            break
-    elif 13 <= int(current_time) <14 or 0<= int(current_time) < 6:
-        now = datetime.now()
-        current_time = now.strftime("%H")
-        print("Current Time =", current_time, "Hour")
-        logger.info('Training Now...')
-        now = datetime.now()
-        current_time = now.strftime("%H")
-        os.system('python tradeAI.py train')
-        if 7 <= int(current_time) < 13:
-            break
+    print("Current Time =", current_time, "Hour")
+    logger.info('Training Now...')
+    now = datetime.now()
+    current_time = now.strftime("%H")
+    os.system('python tradeAI.py train')
+    if 7 <= int(current_time) < 13:
+        break
 
